c78dfaf19c9bddf37e35c299240beed88c947a43
pavlichenko86 <vovan4ik86mail@gmail.com>
2018-01-02 00:42:05 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,10 +5,7 @@ $(document).ready(function(){
 		let link = new URL(per);
 		link = link.hostname.replace(/\.\D*/g, "");
 		
-		link = link.replace(/[oO]/g, 0);
-		link = link.replace(/[l]/g, 1);
-		link = link.replace(/[i]/g, 3);
-		link = link.replace(/[s]/g, 5);
+		link = link.replace(/[oO]/g, 0).replace(/[l]/g, 1).replace(/[i]/g, 3).replace(/[s]/g, 5);
 		let result = link + Math.random().toString(36).slice(2, 6);
 	    $('.url').append("<p>Was: " + per + "</p>" + "<p>Now: " + result + "</p>");
 	});
